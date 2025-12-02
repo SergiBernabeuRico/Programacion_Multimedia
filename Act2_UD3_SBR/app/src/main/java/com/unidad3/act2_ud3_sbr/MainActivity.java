7dc98f96e1707abcc9de4b190fbdb2ec378417e6
SergiBernabeuRico <sergibernabeurico@gmail.com>
2025-12-02 16:56:55 +0100

    Subiendo Act2_UD3_SBR

---
+++ Act2_UD3_SBR/app/src/main/java/com/unidad3/act2_ud3_sbr/MainActivity.java
@@ -0,0 +1,15 @@
+package com.unidad3.act2_ud3_sbr;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Cargamos el layout activity_main con el diseño de la receta
+        setContentView(R.layout.activity_main);
+    }
+}
